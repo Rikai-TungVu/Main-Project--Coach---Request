2d89b4b97889dd1f6b2f7525f5c2d7e10d84019b
Rikai-TungVu <tung.vu@rikai.technology>
2023-01-26 14:02:11 +0900

    Setup router

--- src/router.js
+++ src/router.js
@@ -1,20 +1,27 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
+import CoachDeatail from './pages/coaches/CoachDeatail.vue';
+import CoachList from './pages/coaches/CoachList.vue';
+import CoachRegistation from './pages/coaches/CoachRegistation.vue';
+import ContactCoach from './pages/requests/ContactCoach.vue';
+import RequestReveived from './pages/requests/RequestReveived.vue';
+import NotFound from './pages/NotFound.vue';
+
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', redirect: '/coaches' },
-    { path: '/coaches', component: null },
+    { path: '/coaches', component: CoachList },
     {
       path: '/coaches/:id',
-      component: null,
+      component: CoachDeatail,
       children: [
-        { path: 'contact', component: null }, // coaches/c1/contact
+        { path: 'contact', component: ContactCoach }, // coaches/c1/contact
       ],
     },
-    { path: '/register', component: null },
-    { path: '/requests', component: null },
-    { path: '/:notfound(.*)', component: null },
+    { path: '/register', component: CoachRegistation },
+    { path: '/requests', component: RequestReveived },
+    { path: '/:notfound(.*)', component: NotFound },
   ],
 });
 
