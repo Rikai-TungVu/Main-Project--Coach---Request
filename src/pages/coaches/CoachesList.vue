6c94f3831472cf4c628105a288da03597e19ba73
Rikai-TungVu <tung.vu@rikai.technology>
2023-01-26 14:16:26 +0900

    Setup Router done

---
+++ src/pages/coaches/CoachesList.vue
@@ -0,0 +1,6 @@
+<template>
+  <section>Filter</section>
+  <section>List of coaches</section>
+</template>
+
+<script></script>
--- src/router.js
+++ src/router.js
@@ -1,26 +1,26 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
-import CoachDeatail from './pages/coaches/CoachDeatail.vue';
-import CoachList from './pages/coaches/CoachList.vue';
-import CoachRegistation from './pages/coaches/CoachRegistation.vue';
+import CoachDetails from './pages/coaches/CoachDetails.vue';
+import CoachesList from './pages/coaches/CoachesList.vue';
+import CoachRegistrations from './pages/coaches/CoachRegistrations.vue';
 import ContactCoach from './pages/requests/ContactCoach.vue';
-import RequestReveived from './pages/requests/RequestReveived.vue';
+import RequestsReceived from './pages/requests/RequestsReceived.vue';
 import NotFound from './pages/NotFound.vue';
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', redirect: '/coaches' },
-    { path: '/coaches', component: CoachList },
+    { path: '/coaches', component: CoachesList },
     {
       path: '/coaches/:id',
-      component: CoachDeatail,
+      component: CoachDetails,
       children: [
         { path: 'contact', component: ContactCoach }, // coaches/c1/contact
       ],
     },
-    { path: '/register', component: CoachRegistation },
-    { path: '/requests', component: RequestReveived },
+    { path: '/register', component: CoachRegistrations },
+    { path: '/requests', component: RequestsReceived },
     { path: '/:notfound(.*)', component: NotFound },
   ],
 });
