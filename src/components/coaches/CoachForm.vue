e84d530c9597b565a96f5561327f8b8e020c65eb
Rikai-TungVu <tung.vu@rikai.technology>
2023-01-30 17:28:24 +0900

    register form created

---
+++ src/components/coaches/CoachForm.vue
@@ -0,0 +1,67 @@
+<template>
+  <form @submit.prevent="submitForm">
+    <div class="form-control">
+      <label for="firstname">FirstName</label>
+      <input type="text" id="firstname" v-model.trim="firstName" />
+    </div>
+    <div class="form-control">
+      <label for="lastname">Last Name</label>
+      <input type="text" id="lastname" v-model.trim="lastName" />
+    </div>
+    <div class="form-control">
+      <label for="description">Description</label>
+      <textarea id="description" rows="5" v-model.trim="description"></textarea>
+    </div>
+    <div class="form-control">
+      <label for="rate">Rate</label>
+      <input type="number" id="rate" v-model.number="rate" />
+    </div>
+    <div class="form-control">
+      <h3>Areas of Expertise</h3>
+      <div>
+        <input type="checkbox" id="frontend" value="frontend" v-model="areas" />
+        <label for="frontend">Frontend Devolopment</label>
+      </div>
+      <div>
+        <input type="checkbox" id="backend" value="backend" v-model="areas" />
+        <label for="backend">Frontend Devolopment</label>
+      </div>
+      <div>
+        <input
+          type="checkbox"
+          id="career"
+          value="frocareerntend"
+          v-model="areas"
+        />
+        <label for="career">Career Advisory</label>
+      </div>
+    </div>
+    <base-button>Register</base-button>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      firstName: '',
+      lastName: '',
+      description: '',
+      rate: null,
+      areas: [],
+    };
+  },
+  methods: {
+    submitForm() {
+      const formData = {
+        first: this.firstName,
+        last: this.lastName,
+        desc: this.description,
+        rate: this.rate,
+        areas: this.areas,
+      };
+      console.log(formData);
+    },
+  },
+};
+</script>
--- src/pages/coaches/CoachRegistrations.vue
+++ src/pages/coaches/CoachRegistrations.vue
@@ -1,3 +1,19 @@
-<template>Register</template>
+<template>
+  <section>
+    <base-card>
+      <h2>Register as a coach now!</h2>
+      <CoachForm></CoachForm>
+    </base-card>
+  </section>
+</template>
 
-<script></script>
+<script>
+import CoachForm from '../../components/coaches/CoachForm.vue';
+export default {
+  components: {
+    CoachForm,
+  },
+};
+</script>
+
+<style src="../../css/coach-form.css" />
